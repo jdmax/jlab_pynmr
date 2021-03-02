9d3176a7b110af352787e775645cebe2ce2ec8af
James Maxwell <jmaxwell@jlab.org>
2021-03-02 14:30:33 -0500

    Settings update

--- anapy.bat
+++ anapy.bat
@@ -1,3 +1,3 @@
-@FOR /F "delims=" %%i IN ('@call "C:\Users\%USERNAME%\Anaconda3\Scripts\conda.exe" ..activate "cmd.exe" %1') DO @SET "NEW_PATH=%%i"
+@FOR /F "delims=" %%i IN ('@call "C:\Users\%USERNAME%\Miniconda3\Scripts\conda.exe" ..activate "cmd.exe" %1') DO @SET "NEW_PATH=%%i"
 @SET PATH=%NEW_PATH%;%PATH%
 python %2
